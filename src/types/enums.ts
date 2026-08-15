d3be5a35f7a98087b7031cc3c0c01ea88d7a9e66
Anteneh Wondwosen <antenehwondwosen@gmail.com>
2026-08-15 21:45:05 +0300

    feat(enums): add DataSourceKey, RunStatus, and EvidenceTier enums

---
+++ src/types/enums.ts
@@ -0,0 +1,19 @@
+export enum DataSourceKey {
+  VIIRS = 'VIIRS',
+  GHSL = 'GHSL',
+  RWI = 'RWI',
+  GDELT = 'GDELT',
+}
+
+export enum RunStatus {
+  RUNNING = 'RUNNING',
+  SUCCESS = 'SUCCESS',
+  FAILED = 'FAILED',
+}
+
+export enum EvidenceTier {
+  OFFICIAL = 'OFFICIAL',
+  MARKET_REPORT = 'MARKET_REPORT',
+  INFRASTRUCTURE = 'INFRASTRUCTURE',
+  LOCAL_NEWS = 'LOCAL_NEWS',
+}
